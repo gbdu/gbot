0eecd253f65a5b3e2678d143db1ecf112115409b
gbdu <ogrum@live.com>
2020-08-12 10:23:38 -0600

    !tell nick msg works now (with nedb)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const irc = require('irc');
 const { exec, spawn } = require('child_process');
 const googleIt = require('google-it');
- 
+const Datastore = require('nedb');
+
 // load NICK, PASSWORD, CHANNEL from .env 
 require('dotenv').config();
+
+var db = new Datastore({ filename: 'gb3.db', autoload: true });
+
+// Using a unique constraint with the index
+db.ensureIndex({ fieldName: 'content', unique: true }, function (err) {
+});
 
 var client = new irc.Client(process.env.SERVER, process.env.NICK, {
   channels: [process.env.CHANNELS] || ["#codelove", "#test"],
@@ -60,6 +67,35 @@ client.addListener('message', function(from, to, message) {
 
       }
     });
+  }  
+  else if(message.startsWith("!tell")){
+    let words = message.split(' ');
+    var datetime = new Date();
+    
+    db.count({}, function (err, count) {
+      if(count > 10){
+        client.say(to, "Message db full");
+      }
+      else if(words[1] && words[2]){
+        console.log(words[1].length);
+
+        var msg = {
+          from: from,
+          channel: to,
+          to: words[1],
+          content: words[2],
+          time: datetime.toISOString().slice(0,10),
+        };
+
+        db.insert(msg, function (err){
+          if(!err){
+            client.say(to, "Ok, I'll let them know.");
+          }
+        });
+
+      };
+    
+    });
   }
   else if(message.startsWith("!t")){
     message = message.replace(/-/g, '~');
@@ -85,8 +121,20 @@ client.addListener('message', function(from, to, message) {
 }  );
 
 client.addListener('join', function (channel, who){
-  client.say(channel, who);
+  db.find({ to: who }, function (err, docs) {
+    docs.forEach(d => {
+      client.say(channel, d.to + ": " + d.from + " wanted you to know: " + d.content + " [ on " + d.time + " ]" );
+      db.remove({ _id: d._id }, {}, function (err, numRemoved) {
+        console.log(numRemoved);
+      });
+    }
+    );
+
+    });
+
+    
 });
+
 client.addListener('error', function(message) {
     console.log('error: ', message);
 });
